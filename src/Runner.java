0450c86784945f9167d12a01a1b23355f693ff86
BT_1N3_27 <sample@bths.edu>
2024-10-25 13:18:51 -0400

    added print statement

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World, what's up.");
-        System.out.println("How was your day today? Was anything exciting?");\
+        System.out.println("How was your day today? Was anything exciting?");
         System.out.println("I like your shirt.");
     }
 }
