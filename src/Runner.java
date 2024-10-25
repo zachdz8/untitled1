44e4a9b5cd5d3bab2c55ef151407efc8eba18342
BT_1N3_27 <sample@bths.edu>
2024-10-25 13:16:17 -0400

    added some logic

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("Hello World, what's up.");
+        System.out.println("How was your day today?");
     }
 }
